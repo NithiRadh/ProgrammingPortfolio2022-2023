2cd33ba0428fbb9fafd7b72d1497278946181260
NithiRadh <111790832+NithiRadh@users.noreply.github.com>
2023-02-27 11:32:45 -0700

    Add files via upload

---
+++ src/main (2).py
@@ -0,0 +1,5 @@
+word = input('Please enter words to convert:')
+wordCount = len (word)
+firstLetter = word[0]
+lastPart = word[1:]
+print('your word is: ', lastPart.lower(), firstLetter.lower(), 'ay', sep='')
